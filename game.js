f4f137ab3fb5df35db8e17f7e12e0a9bbd977319
George Mintah <george.mintah@amalitech.org>
2020-10-08 11:00:00 +0100

    wrote JS code and loaded questions

---
+++ game.js
@@ -0,0 +1,48 @@
+const question = document.getElementById("question");
+const choice= Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score =0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question:"Unsolicited e-mails are termed as?"
+    choice1:"Group"
+    choice2:"Spam"
+    choice3:"Virus"
+    choice4:"Internet Protocol"
+    answer: 2
+  }
+
+  {
+    question:"The full form of IRQ is?"
+    choice1:"Interrupt Request"
+    choice2:"Internet Recall Que"
+    choice3:"interest Request Quotation"
+    choice4:"Input Request Quote"
+    answer: 1
+
+  }
+
+  {
+    question:"Cathode Ray Tube is found in?"
+    choice1:"Moniter"
+    choice2:"Keyboard"
+    choice3:"Mother Board"
+    choice4:"Mouse"
+    answer: 1
+  }
+
+  {
+    question:"Among the following, which term is related to memory?"
+    choice1:"WAM"
+    choice2:"MAN"
+    choice3:"CRT"
+    choice4:"RAM"
+    answer: 4
+  }
+]
+
+ 
